7799987a6eedd2e227183382817c9e3f92be4c92
Evilene <victoriadepinheiros@gmail.com>
2025-03-06 16:38:19 +0000

    Add files via upload

---
+++ bandGenerationName.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print("welcome to the band name generation")
+name = input("What is your name?\n")
+city =input(" what city you grew up in?\n")
+pet =input(" What the name of your pet?\n")
+print("Your band name could be: " + city +" " + pet )
